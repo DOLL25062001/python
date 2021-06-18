a731a0d8e23924a816f913b7d0c866eb6c413b07
DOLL25062001 <doll25062001@gmail.com>
2021-06-18 18:16:19 +0530

    enter

---
+++ input.py
@@ -0,0 +1,7 @@
+# user input in python
+text = input("Enter Some Text:")
+print("This is the text i entered is"+" "+text)
+
+# user input where it returns number instead of string
+answer = int(input("How many fingers do you have?:"))
+print(answer)
