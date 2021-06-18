147839aa5a1121138599a49f317b9e66da8789f6
DOLL25062001 <doll25062001@gmail.com>
2021-06-18 18:19:27 +0530

    enter

--- input.py
+++ input.py
@@ -1,7 +1,10 @@
 # user input in python
+
 text = input("Enter Some Text:")
 print("This is the text i entered is"+" "+text)
 
-# user input where it returns number instead of string
+# user input where it returns integer instead of string
+# u can note the difference in output ny removing int
+
 answer = int(input("How many fingers do you have?:"))
-print(answer)
+print(answer*2)
