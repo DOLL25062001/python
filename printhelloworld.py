9face8fa255b1a8c986c34f34ba8e0b5efe26fad
DOLL25062001 <doll25062001@gmail.com>
2021-06-18 11:51:32 +0530

    enter

--- printhelloworld.py
+++ printhelloworld.py
@@ -12,17 +12,3 @@ for n in ascii:
     print(chr(n), end='')
 print("\n")
 print("________________________")
-# print a number ex:34 method1
-x = "Hi you are reading a line to count"
-print(len(x))
-
-# method 2
-
-print("34")
-
-# method 3
-number = 8
-print(len("Hi you are reading a line ")+number)
-
-# method 4 binary form  binary numbers are prefixed with 0b
-print(0b100010)
---
+++ vscode/printnum.py
@@ -0,0 +1,14 @@
+# print a number ex:34 method1
+x = "Hi you are reading a line to count"
+print(len(x))
+
+# method 2
+
+print("34")
+
+# method 3
+number = 8
+print(len("Hi you are reading a line ")+number)
+
+# method 4 binary form  binary numbers are prefixed with 0b
+print(0b100010)
